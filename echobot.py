228a2ede5431e56b9b36ca7c917c0b39766275fb
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2018-11-20 11:00:07 -0200

    v1

--- echobot.py
+++ echobot.py
@@ -1,6 +1,8 @@
 import json 
 import requests
 import os
+import urllib
+import time
 
 TOKEN = os.environ['TELELEGRAM_BOT_TOKEN']
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
@@ -16,10 +18,18 @@ def get_json_from_url(url):
     return js
 
 
-def get_updates():
+def get_updates(offset=None):
     url = URL + "getUpdates"
+    if offset:
+        url += "?offset={}".format(offset)
     js = get_json_from_url(url)
     return js
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
 
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
@@ -28,9 +38,27 @@ def get_last_chat_id_and_text(updates):
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
     return (text, chat_id)
 
-def send_message(text, chat_id):
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-    get_url(url) 
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+def echo_all(updates):
+    for update in updates["result"]:
+        text = update["message"]["text"]
+        chat = update["message"]["chat"]["id"]
+        send_message(text, chat)
+
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
